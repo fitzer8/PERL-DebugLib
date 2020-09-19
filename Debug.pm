47f0cf7acf3bc16a9a7ec1814b621fa05ffbf105
Fitz <fitzer8@gmail.com>
2020-09-19 11:54:15 -0400

    Cleanup

--- Debug.pm
+++ Debug.pm
@@ -2,53 +2,47 @@
 # NAME: Debug.pm
 # FUNC: Code for debugging.
 # AUTH: Fitz
-# VERS: $Id: Debug.pm,v 1.2 2017/10/04 02:12:07 fitzer Exp $
-#  SRC: $Source: /media/fitzer/42478fce-2984-4f76-9d2a-70381ca94d31/src/perl/libs/Debug/RCS/Debug.pm,v $
+# VERS: 0.0.0
+#  SRC: $Source: /home/fitz/src/perl/libs/Debug/Debug.pm
 #===========================================================================
 # HISTORY
 #===========================================================================
 #   DATE      # NAME      # COMMENT
 #===========================================================================
-# 20060805    # Fitz      # Original.
-# 20060805    # Fitz      # Added these comments.
-# 20060805    # Fitz      # Cleanup.
-# 20060805    # Fitz      # Added Debug and Error logs.
-# 20060901    # Fitz      # Added LabeledInt.
-# 20060909    # Fitz      # Small fixes to H2 and H4.
-# 20100222    # Fitz      # Renamed H4 to h4 and added LabeledInt and H4
-# 20130502    # Fitz      # General cleanup.
-# 20150302    # Fitz      # Deprecated LString and LabeledString and
-#													# created lString and lqString.
-# 20150705    # Fitz      # Exit was decrementing IL before the printing.
-# 20150705    # Fitz      # Fixed Entry to increment IL before printing.
-# 20160422    # Fitz      # Added Start and LevelCheck.
-# 20160826    # Fitz      # Added StartBlock and EndBlock.
-# 20171003    # Fitz      # Deprecated LabeledInt and replaced w/ lInt.
+# 20200919    # Fitz      # Cleanup.
 ############################################################################
 ############################################################################
-# sub Indent {
-# sub Nl {
-# sub Char {
-# sub QChar {
-# sub Debug {
-# sub Label {
-# sub String
-# sub QString
-# sub LabeledString {
-# sub Int {
-# sub LabeledInt {
-# sub Hex {
-# sub H2 {
-# sub H4 {
-# sub Entry {
-# sub Exit {
-# sub LString {
-# sub lString {
-# sub lqString {
-# sub Ntry {
-# sub Spaces {
-# sub StartBlock {
-# sub EnbdBlock {
+#sub Indent {
+#sub indentString {
+#sub Nl {
+#sub Char {
+#sub QChar {
+#sub Debug {
+#sub Label {
+#sub String
+#sub QString
+#sub LabeledString {
+#sub Int {
+#sub LabeledInt {
+#sub lInt {
+#sub Hex {
+#sub H2 {
+#sub H4 {
+#sub Entry {
+#sub Exit {
+#sub LString {
+#sub lString {
+#sub lqString {
+#sub Ntry {
+#sub Spaces {
+#sub Start {
+#sub LevelCheck {
+#sub StartBlock {
+#sub EndBlock {
+#sub ntry {
+#sub xit {
+#sub Log {
+#sub DebugLevels {	# Formerly Start
 ############################################################################
 use Data::Dumper;
 
@@ -83,6 +77,19 @@ sub Indent {
 		}
 	}
 	undef $n;
+}
+
+sub indentString {
+	my( $n ) = $IL;
+	my $str = "";
+
+	if( $n > 0 ) {
+		while( $n > 0 ) {
+			$str .= "  ";
+			$n--;
+		}
+	}
+	return $str;
 }
 
 sub Nl {
@@ -325,9 +332,84 @@ sub EndBlock {
 	return $il;
 }
 
+sub ntry {
+	my( $file ) = shift( @_ );
+	my( $proc ) = shift( @_ );
+
+	Indent( $IL );
+	print( "Entering: \"$proc\" in File: \"$file\"\n" );
+	$IL++;
+}
+
+ 
+sub xit {
+	my( $file ) = shift( @_ );
+	my( $proc ) = shift( @_ );
+
+	--$IL;
+	Indent( $IL );
+	print( "Exiting \"$proc\" in File: \"$file\"\n" );
+}
+
+ 
+sub Log {
+	 my( $tmp ) = shift( @_ );
+	 my $ind = indentString();
+
+	 print $ind;
+	 if( $tmp eq "Entry" ) {
+		 $txt = "Entering proc: ";
+	 } elsif( $tmp eq "Exit" ) {
+		 $txt = "Exiting proc: ";
+	 } else {
+		 $txt = uc $tmp;
+		 #$txt = "$txt - ";
+	 }
+	 print "$txt - ";
+	 print @_;
+	 print "\n";
+
+	 print $LOG $ind;
+	 print $LOG "$txt - @_ \n";
+}
+
+ 
+sub DebugLevels {	# Formerly Start
+	my $DBG = 0;
+	if( $DBG )
+		{ print "Entering DebugLevels\n"; }
+	my( $n ) = shift( @_ );
+	my( $proc ) = shift( @_ );
+ 
+	if( $DBG ) {
+		print "n: $n\n";
+		print "proc: $proc\n";
+	}
+ 
+	while( $n ) {
+		my ( $n1, $rest ) = split( /,/, $n, 2 );
+		my $str = $proc . $n1;
+		$Levels{ $str } = $n1;
+		$n = $rest;
+ 
+		if( $DBG ) {
+			print "n1: $n1\n";
+			print "Levels: str: $str\n";
+			print Dumper( \%Levels );
+		}
+	}
+ 
+	if( $DBG )
+		{ print "Exiting DebugLevels\n"; }
+} # sub DebugLevels
+ 
+ 
 #print "Fitz\n";
 
 1;
 
 
+############################################################################
+############################################################################
+############################################################################
 
